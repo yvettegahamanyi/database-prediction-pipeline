51e3bafc330103ee6107fc64826c29ccb1d0b563
yvettegahamanyi <ygahamanyi26@gmail.com>
2025-11-01 19:43:28 +0200

    ft: mongo db config

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ import uvicorn
 
 app = FastAPI(title="Heart Disease API")
 
-app.include_router(health_indicators_router)
-
 @app.get("/")
 def root():
     return {"message": "Heart Disease API is running"}
+app.include_router(health_indicators_router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
--- routes/pg_health_indicators.py
+++ routes/pg_health_indicators.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from formative_1_ml_pipeline.database import SessionLocal
-from formative_1_ml_pipeline.crud.crud_health_indicators import (
+from formative_1_ml_pipeline.database.postgres_db import SessionLocal
+from formative_1_ml_pipeline.crud.pg_crud_health_indicators import (
     create_health_indicator as create_indicator_crud,
     get_health_indicators as get_indicators_crud,
     update_health_indicator as update_indicator_crud,
